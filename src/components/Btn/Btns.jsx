35eee0541b99685cd98c3eedbcfe5c0a95740858
Ahmed <mnaz004587@gmail.com>
2024-09-03 20:23:21 +0300

    change-btn

--- src/components/Btn/Btns.jsx
+++ src/components/Btn/Btns.jsx
@@ -1,85 +1,94 @@
 /* eslint-disable react/prop-types */
 // import { useState } from "react";
 // import { useState } from 'react';
-import { useState } from 'react';
-import './Btn.css';
+import { useState } from "react";
+import "./Btn.css";
 function Btns({ setId }) {
   const [activeButton, setActiveButton] = useState(0);
-    return (
-      <div className="btn-container">
-        <button
-          onClick={() => {
-            setId(0);
-            setActiveButton(0);
-          }}
-          className={activeButton === 0 ? "active" : ""}
-        >
-          <span>All</span>
-        </button>
-    
-        <button
-          onClick={() => {
-            setId(28);
-            setActiveButton(28);
-          }}
-          className={activeButton === 28 ? "active" : ""}
-        >
-          <span>Action</span>
-        </button>
-    
-        <button
-          onClick={() => {
-            setId(16);
-            setActiveButton(16);
-          }}
-          className={activeButton === 16 ? "active" : ""}
-        >
-          <span>Animation</span>
-        </button>
-    
-        <button
-          onClick={() => {
-            setId(18);
-            setActiveButton(18);
-          }}
-          className={activeButton === 18 ? "active" : ""}
-        >
-          <span>Drama</span>
-        </button>
-    
-        <button
-          onClick={() => {
-            setId(80);
-            setActiveButton(80);
-          }}
-          className={activeButton === 80 ? "active" : ""}
-        >
-          <span>Crime</span>
-        </button>
-    
-        <button
-          onClick={() => {
-            setId(10749);
-            setActiveButton(10749);
-          }}
-          className={activeButton === 10749 ? "active" : ""}
-        >
-          <span>Romance</span>
-        </button>
-    
-        <button
-          onClick={() => {
-            setId(9648);
-            setActiveButton(9648);
-          }}
-          className={activeButton === 9648 ? "active" : ""}
-        >
-          <span>Mystery</span>
-        </button>
-      </div>
-    );
+  return (
+    <div className="btn-container">
+      <button
+        onClick={() => {
+          setId(0);
+          setActiveButton(0);
+        }}
+        className={activeButton === 0 ? "active" : ""}
+      >
+        <span>All</span>
+      </button>
 
+      <button
+        onClick={() => {
+          setId(28);
+          setActiveButton(28);
+        }}
+        className={activeButton === 28 ? "active" : ""}
+      >
+        <span>Action</span>
+      </button>
 
+      <button
+        onClick={() => {
+          setId(16);
+          setActiveButton(16);
+        }}
+        className={activeButton === 16 ? "active" : ""}
+      >
+        <span>Animation</span>
+      </button>
+
+      <button
+        onClick={() => {
+          setId(18);
+          setActiveButton(18);
+        }}
+        className={activeButton === 18 ? "active" : ""}
+      >
+        <span>Drama</span>
+      </button>
+
+      <button
+        onClick={() => {
+          setId(80);
+          setActiveButton(80);
+        }}
+        className={activeButton === 80 ? "active" : ""}
+      >
+        <span>Crime</span>
+      </button>
+
+      <button
+        onClick={() => {
+          setId(36);
+          setActiveButton(36);
+        }}
+        className={activeButton === 36? "active" : ""}
+      >
+        <span>History</span>
+      </button>
+
+      <button
+        onClick={() => {
+          setId(9648);
+          setActiveButton(9648);
+        }}
+        className={activeButton === 9648 ? "active" : ""}
+      >
+        <span>Mystery</span>
+      </button>
+
+      <button
+        onClick={() => {
+          setId(1);
+          setActiveButton(1);
+        }}
+        className={activeButton === 1 ? "active" : ""}
+      >
+        <span>Top Rated</span>
+      </button>
+
+    </div>
+  );
 }
 
 export default Btns;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,31 +4,44 @@ import { useEffect, useState } from "react";
 import "./Products.css";
 import { API_KEY } from "../../data";
 import { Link } from "react-router-dom";
-const Products = ({ state, statePage, setTotalPages , search }) => {
+const Products = ({ state, statePage, setTotalPages, search }) => {
   // console.log(state,statePage);
   const [movies, getData] = useState([]);
   useEffect(() => {
-    (search.length > 0 ) ? fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}&include_adult=false&include_video=false&language=en-US&page=${statePage}&sort_by=popularity.desc${
-          state && `&with_genres=${state}`
-        }`).then((res) => res.json())
-      .then((data) => {
-        setTotalPages(data.total_pages);
-        getData(data.results);
-      }) : fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=${statePage}&sort_by=popularity.desc${
-          state && `&with_genres=${state}`
-        }`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          setTotalPages(data.total_pages);
-          getData(data.results);
-        }) 
-
+    search.length > 0
+      ? fetch(
+          `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${search}&include_adult=false&include_video=false&language=en-US&page=${statePage}&sort_by=popularity.desc${
+            state && `&with_genres=${state}`
+          }`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            setTotalPages(data.total_pages);
+            getData(data.results);
+          })
+      : state == 1
+      ?  fetch( `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=${statePage}`)
+          .then((res) => res.json())
+          .then((data) => {
+            setTotalPages(data.total_pages);
+            getData(data.results);
+          })
+      : fetch(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=${statePage}&sort_by=popularity.desc${
+            state && `&with_genres=${state}`
+          }`
+        )
+          .then((res) => res.json())
+          .then((data) => {
+            setTotalPages(data.total_pages);
+            getData(data.results);
+          });
   }, [state, statePage, search]);
-//  console.log(movies);
+  console.log(movies);
+  // fetch( `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&language=en-US&page=1`)
+  //  console.log(movies);
   // https://api.themoviedb.org/3/search/collection?api_key=YOUR_API_KEY&query=NAME
-          
+
   // https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&include_adult=false&include_video=false&language=en-US&page=3&sort_by=popularity.desc&with_genres=${state}
 
   //https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc
@@ -46,7 +59,7 @@ const Products = ({ state, statePage, setTotalPages , search }) => {
         return (
           <div key={el.id} className="card">
             <div className="img-div">
-              <img src={`${"https://image.tmdb.org/t/p/w500" + el.poster_path}`}  alt="No Image"/>
+              <img src={`${"https://image.tmdb.org/t/p/w500" + el.poster_path}`} alt="No Image" />
             </div>
             <h3>{`${el.title.length < 30 ? el.title : el.title.slice(0, 30) + "..."}`}</h3>
             <p>{`${el.overview.slice(0, 40)}...`}</p>
