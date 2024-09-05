e9c2a61abe6a2e478286c77855ad0e3d7e19f21b
Ahmed <mnaz004587@gmail.com>
2024-09-05 14:25:00 +0300

    change-some-details

--- src/Details/DetailsProduct.jsx
+++ src/Details/DetailsProduct.jsx
@@ -8,6 +8,7 @@ import { useData } from "../components/context/globalContext";
 function DetailsProduct() {
   const [video, setVideo] = useState({});
   const [trailer, setTrailer] = useState([]);
+  const [cast, setCast] = useState([]);
   const { id } = useParams();
   console.log(id);
   useEffect(() => {
@@ -22,8 +23,19 @@ function DetailsProduct() {
         const trailerVideo = data.results.find((video) => video.type === "Trailer");
         setTrailer(trailerVideo);
       });
+      fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=${API_KEY}&language=en-US`)
+      .then(response => response.json())
+      .then(data => {
+        const cast = data.cast.slice(0,8).map(member => ({
+          name: member.name,
+          character: member.character,
+          profilePath: `https://image.tmdb.org/t/p/w500${member.profile_path}`, // URL للصورة
+        }));
+        setCast(cast)
+        console.log(cast);
+      })      
   }, [id]);
-  console.log(video);
+  // console.log(video);
 
   const rating = (video.vote_average * 10).toFixed(1);
   const releaseYear = video.release_date;
@@ -72,7 +84,16 @@ function DetailsProduct() {
               })
             }}><span>AddToWatchList</span></button>
      </div>
-    </div>
+      <p style={{padding:"10px"}}>Cast</p>
+     <div className="div-img-cast" style={{display:"flex",alignItems:"center",justifyContent:"space-around",gap:"5px",flexWrap:"wrap"}}>
+     {
+      cast.map((member,idx)=>{
+         return (member.profilePath && <img key={idx} src={member?.profilePath} alt={member.name} style={{ width: '100px', borderRadius: '10px', margin:"5px" }} />)    
+      })
+     }
+
+     </div>
+         </div>
   );
 }
 
